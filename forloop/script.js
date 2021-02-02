c132c7bff501abff5876e37e3054289c2ced419a
katpan75 <kpan@risd.edu>
2021-02-02 11:03:48 -0500

    name guessing

--- forloop/script.js
+++ forloop/script.js
@@ -1,11 +1,12 @@
 let x = ["rose","Daisy","violet","Iris"];
 
-let yes = document.getElementsByClassName("answeryes");
-let no = document.getElementsByClassName("answerno");
+let guess = document.getElementById("#guess");
+let result = document.getElementById("yourguess");
+
+guess.addEventListener('input', function () { result.textContent = this.value;});
+
 
 function myfunction(){
-    let N = document.getElementById("guess").value;
-    console.log(N);
     for(i=0; i<x.length; i++){
       if( N == x[i]){
             yes.setAttribute("style","display:block;");
