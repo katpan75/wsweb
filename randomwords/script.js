cb0c0a4dade853d6c996ffccc76e6038b40d4113
katpan75 <kpan@risd.edu>
2021-02-12 11:38:47 -0500

    added exercise

--- randomwords/script.js
+++ randomwords/script.js
@@ -1,5 +1,5 @@
 function namefunction(){
-    var names = ['Mary', 'Tom', 'Jack'];
+    var names = ['Mary', 'Tom', 'Jack','Eddie','Henry','Yuki','Lee'];
     var number = Math.floor(Math.random() * names.length);
     var name = names[number]
     return name;
